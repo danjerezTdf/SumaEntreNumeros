a4605733c42077542d045173366d9f576cf3b3a8
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:27:34 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/* Suma entre Números
+• Escriba un programa que pida al usuario dos números
+enteros, y luego retorne la suma de todos los números
+que están entre ellos
+• Por ejemplo, si los números son 2 y 7, debe entregar
+como resultado 2 + 3 + 4 + 5 + 6 + 7 = 27 */
+
 let valorUno: number = Number(prompt(`ingrese primer Numero:`));
 let valorDos: number = Number(prompt(`ingrese segundo Numero`));
 let auxUno: number = 0;
